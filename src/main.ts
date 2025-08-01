5c7458f38f4d3cb3471c59c704f59842ad937ffc
IHaveAName2653 <89300269+IHaveAName2653@users.noreply.github.com>
2025-08-01 14:49:24 +1000

    g?

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { invoke } from "@tauri-apps/api/core";
 import { getCurrentWebview } from "@tauri-apps/api/webview";
 
-const InputField = document.getElementById("ImageInput");
-const PreviewImage = document.getElementById("PreviewImage");
-const ProcessButton = document.getElementById("Process");
-const Canvas = document.getElementById("Canvas");
-const OutputImage = document.getElementById("OutputImage");
+const InputField = document.getElementById("ImageInput")! as HTMLInputElement;
+const PreviewImage = document.getElementById("PreviewImage")! as HTMLImageElement;
+const ProcessButton = document.getElementById("Process")! as HTMLButtonElement;
+const Canvas = document.getElementById("Canvas")! as HTMLCanvasElement;
+const OutputImage = document.getElementById("OutputImage")! as HTMLImageElement;
 
 const stage = Jcrop.attach("PreviewImage");
 
@@ -13,9 +13,9 @@ InputField.addEventListener("drop", (e) => {
 	console.log(e);
 	e.preventDefault();
 	const reader = new FileReader();
-	reader.readAsDataURL(e.dataTransfer.files[0]);
+	reader.readAsDataURL(e.dataTransfer!.files[0]);
 	reader.onload = () => {
-		PreviewImage.src = reader.result;
+		PreviewImage.src = reader.result as string;
 	};
 	stage.removeWidget(stage.active);
 });
@@ -23,18 +23,18 @@ InputField.addEventListener("change", (e) => {
 	console.log(e);
 	e.preventDefault();
 	const reader = new FileReader();
-	reader.readAsDataURL(e.target.files[0]);
+	reader.readAsDataURL(e.target!.files[0]);
 	reader.onload = () => {
-		PreviewImage.src = reader.result;
+		PreviewImage.src = reader.result as string;
 	};
 	stage.removeWidget(stage.active);
 });
-ProcessButton.onclick = (ev) => {
+ProcessButton.onclick = () => {
 	const img = new Image();
 	img.src = PreviewImage.src;
 	Canvas.width = stage.active.pos.w;
 	Canvas.height = stage.active.pos.h;
-	const ctx = Canvas.getContext("2d");
+	const ctx = Canvas.getContext("2d")!;
 
 	ctx.drawImage(img, -stage.active.pos.x, -stage.active.pos.y);
 
@@ -45,8 +45,7 @@ ProcessButton.onclick = (ev) => {
 await getCurrentWebview().onDragDropEvent(async (event) => {
 	if (event.payload.type === "drop") {
 		const filepath = event.payload.paths[0];
-		const output = await invoke("get_image_from_path", { name: filepath });
-		stage.removeWidget(stage.active);
+        const output : string = await invoke("get_image_from_path", { name: filepath });
 		PreviewImage.src = output;
 	}
 });
